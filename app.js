1275b2793caab6fe191a4d3a61c742ad256805ff
Seetharam1999 <38151089+Seetharam1999@users.noreply.github.com>
2020-11-04 13:00:05 +0530

    Update app.js
    cors origin

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
+  //res.header("Access-Control-Allow-Credentials", true);
+ 
+ 
+})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
